b84ce4a8fa8a2c190e95b755a87053dd0a95675a
Dextron <61046945+Dextron04@users.noreply.github.com>
2022-03-08 11:00:47 -0800

    Add files via upload
    Class Files in ASMT 02

---
+++ Receipt.java
@@ -0,0 +1,47 @@
+/*
+ * **********************************************
+ * San Francisco State University
+ * CSC 220 -  Data Structures
+ * File Name: Timer.java
+ * @author: Duc Ta
+ * @author: Tushin Kulshreshtha
+ * @partner: Riken Kapadia
+ * **********************************************
+ */
+
+package assignment02PartB;
+// Please organize all the given files in 1 same package
+// Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
+
+public final class Receipt {
+
+    //
+    // Static Data Fields
+    //
+
+    //
+    // Instance Data Fields
+    //
+
+    //
+    // Constructors
+    //
+    public Receipt() {
+    }
+
+    //
+    // Static Methods
+    //
+
+    //
+    // Instance Methods
+    //
+
+    //
+    // Additional Methods
+    //
+
+    //
+    // Language
+    //
+}
